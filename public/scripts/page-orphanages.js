8776f19beaf7ea2d65f0a46b264a3e851366a79a
Valdir Moura <lucasfarias.bsb@hotmail.com>
2020-10-13 10:53:43 -0300

    orphanages

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -19,7 +19,7 @@ const popup = L.popup({
   className: 'map-popup',
   minWidth: 240,
   minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src=".public/images/arrow-white.svg" > </a>')
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="../public/images/arrow-white.svg" > </a>')
 
 // create and add marker
 L.marker([-15.7750836,-48.0772927], {icon}).addTo(map)
