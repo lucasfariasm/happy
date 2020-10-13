85dd395fe169ba52b1c028915e50d0855b446d33
Valdir Moura <lucasfarias.bsb@hotmail.com>
2020-10-13 11:57:20 -0300

    continuação

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,33 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false
+}
+//Create map 
+const map = L.map('mapid', options).setView([-15.7750836,-48.0772927], 10);
+
+//create and add tileLayer
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+})
+
+
+// create and add marker
+L.marker([-15.7750836,-48.0772927], {icon}).addTo(map)
+
+// image gallery
+
+function selectImage(event){
+  const button = event.currentTarget
+
+  // remover todas as classes active
+}
