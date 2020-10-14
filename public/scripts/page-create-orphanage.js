10d488b19a5071c37523f2ae756caa595aa3608a
Valdir Moura <lucasfarias.bsb@hotmail.com>
2020-10-14 17:15:52 -0300

    adicionando foto

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -30,3 +30,27 @@ map.on('click',(event) => {
   .addTo(map)
 })
 
+
+// photos upload
+function addPhotoField(){
+  // pegar o container de fotos #images
+  const container = document.querySelector('#images')
+  // pegar o container para duplicar .new-image
+  const fieldsContainer = document.querySelectorAll('.new-upload');
+  // realizar o clone da última imagem adicionada
+  const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+  // Verificar se vazio. Se sim, não adicionar ao container de imagens
+
+  const input = newFieldContainer.children[0]
+
+  if (input.value == "") {
+    return 
+  }
+
+  // Limpar campo
+  input.value = "";
+  // adicionar o clone ao container de #images
+  container.appendChild(newFieldContainer)
+}
+
